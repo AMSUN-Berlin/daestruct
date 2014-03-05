e47a755a525451124975f65a3e7d31494cb1a543
Christoph Höger <christoph.hoeger@tu-berlin.de>
2014-03-05 14:58:11 +0100

    Add preliminary compression optimization
    Signed-off-by: Christoph Höger <christoph.hoeger@tu-berlin.de>

--- include/daestruct/analysis.hpp
+++ include/daestruct/analysis.hpp
@@ -34,12 +34,78 @@ namespace daestruct {
 			   const sigma_matrix& sigma,
 			   std::vector<int>& c, std::vector<int>& d);
 
+    struct InflatedMap {
+      /* public variables and non-component equations */
+      std::vector<int> cols;
+      std::vector<int> rows;
+      
+      /* component-private variables and component-equations */
+      std::vector<int> component_cols;
+      std::vector<int> component_rows;
+    };
+
     struct AnalysisResult {
       std::vector<int> row_assignment;
       std::vector<int> col_assignment;
       
       std::vector<int> c;
       std::vector<int> d;
+
+      InflatedMap inflated;
+
+      int extracted_equation(int eq, int k) const ;
+
+      int extracted_variable(int var, int k) const;
+    };
+
+    /**
+     * A compressible sub-component S = ((P U Q) U X, D, d, {M_q})
+     * Q = {0 .. q - 1}
+     * P = {0 .. p - 1}
+     * X = {0 .. p}
+     */
+    struct compressible {
+      int p;
+      int q;
+
+      //TODO: replace this with a better suited matrix, 
+      //every row from p+1 .. p+q will be full in the end
+      sigma_matrix sigma; //represents D and d as well
+
+      /* M_q in the form of row-assignments */
+      std::vector<std::vector<int>> M;
+
+      /* cost of the M_q assignments */
+      std::vector<int> cost;
+
+      compressible(int pub_v, int pri_v) : p(pri_v), q(pub_v), sigma(p+q) {}
+
+      /**
+       * Seal this sub-component (i.e. afterwards set_incidence is a no-op)
+       */
+      void seal();
+
+      /**
+       * Set the incidence in this sub component
+       * Note: i < p + 1 !
+       */
+      void set_incidence(int i, int j, int val);
+    };
+
+    struct compressible_instance {
+      int q; /* offset of the first public variable in the compressed matrix */
+      int s; /* the surrogat equation */    
+      compressible* c;
+
+      compressible_instance(int qv, int se, compressible* comp) : q(qv), s(se), c(comp) {}
+
+      void insert_incidence(sigma_matrix& sigma) const;
+    };
+
+    struct compression {
+      int variables() const;
+
+      std::vector<compressible_instance> instances;
     };
 
     struct InputProblem {
@@ -49,6 +115,8 @@ namespace daestruct {
       InputProblem(long d) : dimension(d), sigma(d) {}
 
       AnalysisResult pryceAlgorithm() const;
+    
+      AnalysisResult pryceCompressed(const compression& c) const;
     };
   }
 }
--- src/analysis.cpp
+++ src/analysis.cpp
@@ -102,5 +102,153 @@ namespace daestruct {
 
       return result;
     }
+
+    sigma_matrix copy_defrag_noninflated(const sigma_matrix& compressed, const solution& comp_assignment,
+					 AnalysisResult& result, const compression& c) {
+
+      sigma_matrix inflated ( result.row_assignment.size() );
+      int si = 0;
+      
+      for (auto row_iter = compressed.rowBegin(); row_iter != compressed.rowEnd(); row_iter++) {
+	const int i = row_iter.index1();
+	if (i != c.instances[si].s) {
+	  /* copy row */
+	  for (auto col_iter = row_iter.begin(); col_iter != row_iter.end(); col_iter++)
+	    inflated.insert(si, col_iter.index2(), *col_iter);
+	  
+	  /* copy assignment */
+	  result.row_assignment[i - si] = comp_assignment.rowsol[i];
+	  result.col_assignment[comp_assignment.rowsol[i]] = i - si;
+	} else {
+	  si++;
+	}
+      }
+
+      return inflated;
+    }
+
+    void inflate(sigma_matrix& inflated, const solution& comp_assignment, AnalysisResult& result, const compression& c) {
+      int row_offset = comp_assignment.rowsol.size() - c.instances.size();
+      int col_offset = comp_assignment.colsol.size();
+
+      /* add private variables and equations */
+      for (const compressible_instance& inst : c.instances) {
+	/* which public variable does this component solve ? */
+	const int k = comp_assignment.rowsol[inst.s] - inst.q;
+	for (auto row_iter = inst.c->sigma.rowBegin(); row_iter != inst.c->sigma.rowEnd(); row_iter++)
+	  for (auto col_iter = row_iter.begin(); col_iter != row_iter.end(); col_iter++) {
+	    const int i = row_iter.index1() + row_offset;
+	    const int j = 	    
+	      (col_iter.index2() > inst.c->q) ?  // private variable
+	      col_iter.index2() + col_offset
+	      : 
+	      col_iter.index2() + inst.q; //public var
+	    ;
+
+	    inflated.insert(i, j, *col_iter);
+
+	    /* get the required matching */
+	    const std::vector<int>& M = inst.c->M[k];
+
+	    /* inflate matching as well */
+	    const int i_m = M[row_iter.index1()];
+	    if (i_m > inst.c->q) {
+	      result.row_assignment[i] = i_m + col_offset;
+	      result.col_assignment[i_m + col_offset] = i;
+	    } else {
+	      result.row_assignment[i] = i_m + inst.q;
+	      result.col_assignment[i_m + inst.q] = i;
+	    }
+	  }
+
+	/* book keeping */
+	result.inflated.component_cols.push_back(col_offset);
+	result.inflated.component_rows.push_back(row_offset);
+
+	row_offset += inst.c->p + 1; //advance equations
+	col_offset += inst.c->p; // advance variables
+      }
+    }
+
+    AnalysisResult InputProblem::pryceCompressed(const compression& c) const {
+      /* solve linear assignment problem */
+      solution comp_assignment = lap(sigma);
+      AnalysisResult result;
+      result.row_assignment.resize(dimension + c.variables());
+      result.col_assignment.resize(dimension + c.variables());
+      
+      sigma_matrix inflated = copy_defrag_noninflated(sigma, comp_assignment, result, c);
+
+      inflate(inflated, comp_assignment, result, c);
+
+      result.c.resize(dimension + c.variables());
+      result.d.resize(dimension + c.variables());
+      
+      solveByFixedPoint(result.row_assignment, inflated, result.c, result.d);
+      
+      return result;
+    }
+          
+    int AnalysisResult::extracted_equation(int eq, int k) const {
+      return inflated.component_rows[k] + eq;
+    }
+
+    int AnalysisResult::extracted_variable(int var, int k) const {
+      return inflated.component_cols[k] + var;
+    }
+    
+    /**
+     * Seal this sub-component (i.e. afterwards set_incidence is a no-op)
+     */
+    void compressible::seal() {
+      if (M.size() == 0) {
+	for (int j = p; j < p+q; j++) {
+	  /* prepare identity matrix for the remaining rows and public vars */
+	  for (int i = p+1; i < p+q; i++)
+	    for (int j2 = p; j2 < p+q; j2++)
+	      if (j2 != j)
+		sigma.insert(i,j2,0);
+
+	  solution sol = lap(sigma);
+	  std::vector<int> M_i;
+	  M_i.reserve(p+1);
+	  for (int i = 0; i < p+1; i++)
+	    M_i[i] = sol.rowsol[i];
+	  M.push_back(M_i);
+	  cost.push_back(sol.cost);
+
+	  /* reset the identity matrix for the next run */
+	  for (int i = p+1; i < p+q; i++)
+	    for (int j2 = 0; j2 < q; j2++)
+	      if (j2 != j)
+		sigma.insert(i,j2,BIG);
+	}	
+      }
+    }
+
+    /**
+     * Set the incidence in this sub component
+     * Note: i < p + 1 !
+     */
+    void compressible::set_incidence(int i, int j, int val) {
+      if (M.size() == 0) {
+	sigma.insert(i, j, val);
+      }
+    }
+
+    void compressible_instance::insert_incidence(sigma_matrix& sigma) const {
+      c->seal();
+      for (int j = 0; j < c->q; j++)
+	sigma.insert(s, j + q, c->cost[j]);
+    }
+
+
+    int compression::variables() const {
+      int vs = 0;
+      for (const auto& inst : instances) 
+	vs += inst.c->p;
+      return vs;
+    }
+
   }
 }
