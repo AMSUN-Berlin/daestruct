AMSUN-Berlin/daestruct

commit f309de6d89e91cec99b912e6f0a8c231cb09b675

Allow multiple insertions in matrix
Just in case...

Signed-off-by: Christoph Höger <christoph.hoeger@tu-berlin.de>

include/daestruct/sigma_matrix.hpp
@@ -71,12 +71,16 @@ namespace daestruct {
     }
 
     void insert(int i, int j, int x) {
-      const int* ptr = m.find_element(minimum_row[j],j);
+      int* ptr = m.find_element(minimum_row[j],j);
       
-      if (!ptr || *ptr > x)
+      if (!ptr) {
 	minimum_row[j] = i;
-
-      m.insert_element(i,j,x);
+	m.insert_element(i,j,x);
+      } else {
+	if (*ptr > x)
+	  minimum_row[j] = i;	  
+	*ptr = x;
+      }
     }
   
     template<class E, class T> inline static void nicePrint(std::basic_ostringstream<E, T>& s, int val) {

commit 3388c665e3a6d25e31c0f1db935b254128b42dca

Fix regression introduced in f309de6d89e91cec99b912e6f0a8c231cb09b675
I should not commit just before vacation ...

Signed-off-by: Christoph Höger <christoph.hoeger@tu-berlin.de>

include/daestruct/sigma_matrix.hpp
@@ -71,14 +71,16 @@ namespace daestruct {
     }
 
     void insert(int i, int j, int x) {
-      int* ptr = m.find_element(minimum_row[j],j);
+      const int* m_ptr = m.find_element(minimum_row[j],j);
       
-      if (!ptr) {
+      if (!m_ptr || *m_ptr > x) {
 	minimum_row[j] = i;
+      }
+      
+      int* ptr = m.find_element(i, j);
+      if (!ptr) {
 	m.insert_element(i,j,x);
       } else {
-	if (*ptr > x)
-	  minimum_row[j] = i;	  
 	*ptr = x;
       }
     }